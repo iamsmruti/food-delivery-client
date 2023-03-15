fd75f189c515ad575497e1da33049a428bc5d40b
preritjn <preritbgr3010@gmail.com>
2023-03-15 20:10:39 +0530

    initial commit

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/Screens/LoginScreen.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
+    home: LoginScreen(),
   ));
 }
