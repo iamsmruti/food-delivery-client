252b1306ecb6864ac0ff4ecd411c4d561ede9bf8
Dibyacoder <b421020@iiit-bh.ac.in>
2023-06-12 21:59:16 +0530

    User_side screen completed

---
+++ frontend/lib/Custom UI/small_font.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SmallFont extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  double height;
+  TextOverflow overflow;
+  FontWeight? fontWeight;
+
+  SmallFont(
+      {super.key,
+      this.color = const Color(0xFF332d2b),
+      required this.text,
+      this.overflow = TextOverflow.ellipsis,
+      this.size = 15,
+      this.height = 1.4,
+      this.fontWeight = FontWeight.w600});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: GoogleFonts.poppins(
+        textStyle: TextStyle(
+          color: color,
+          fontWeight: fontWeight,
+          fontSize: size,
+          height: height,
+          overflow: null,
+        ),
+      ),
+    );
+  }
+}
